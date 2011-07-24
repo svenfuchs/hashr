fc993d234b7ec5b4de54d5b59e1d704d68a8bb34
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-24 18:49:37 +0200

    don't use a constant here as this clashes with I18n (should be fixed over there, too)

--- lib/hashr/core_ext/ruby/hash.rb
+++ lib/hashr/core_ext/ruby/hash.rb
@@ -18,11 +18,8 @@ class Hash
   end unless Hash.method_defined?(:deep_symbolize_keys)
 
   # deep_merge_hash! by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
-  MERGER = proc do |key, v1, v2|
-    Hash === v1 && Hash === v2 ? v1.merge(v2, &MERGER) : v2
-  end
-
   def deep_merge(other)
-    merge(other, &MERGER)
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    merge(other, &merger)
   end unless Hash.method_defined?(:deep_merge)
 end
