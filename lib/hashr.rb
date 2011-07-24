70cff41241dcf875c7f172656e5d0bd40580f655
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-24 17:19:10 +0200

    remove read method

--- lib/hashr.rb
+++ lib/hashr.rb
@@ -35,16 +35,12 @@ class Hashr < Hash
     when '='
       self[name.to_s[0..-2].to_sym] = args.first
     else
-      read(name)
+      raise(IndexError.new("Key #{name.inspect} is not defined.")) if !key?(name) && self.class.raise_missing_keys
+      self[name]
     end
   end
 
   protected
-
-    def read(key)
-      raise(IndexError.new("Key #{key.inspect} is not defined.")) if !key?(key) && self.class.raise_missing_keys
-      self[key]
-    end
 
     def include_modules(modules)
       Array(modules).each { |mod| meta_class.send(:include, mod) } if modules
