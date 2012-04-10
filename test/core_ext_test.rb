1107d85931a447aa50ab78f8802e4c46e1391995
Sven Fuchs <svenfuchs@artweb-design.de>
2012-04-11 01:51:50 +0200

    fix Hash#slice to not explode on missing keys

--- test/core_ext_test.rb
+++ test/core_ext_test.rb
@@ -1,16 +1,28 @@
 require 'test_helper'
 
 class CoreExtTest < Test::Unit::TestCase
-  test 'deep_symbolize_keys walks arrays, too' do
+  test 'Hash#deep_symbolize_keys walks arrays, too' do
     hash     = { 'foo' => [{ 'bar' => 'bar', 'baz' => { 'buz' => 'buz' } }] }
     expected = { :foo  => [{ :bar  => 'bar', :baz  => { :buz  => 'buz' } }] }
     assert_equal expected, hash.deep_symbolize_keys
   end
 
-  test 'deep_symbolize_keys! replaces with deep_symbolize' do
+  test 'Hash#deep_symbolize_keys! replaces with deep_symbolize' do
     hash     = { 'foo' => { 'bar' => 'baz' } }
     expected = { :foo => { :bar => 'baz' } }
     hash.deep_symbolize_keys!
     assert_equal expected, hash
+  end
+
+  test 'Hash#slice returns a new hash containing the given keys' do
+    hash = { :foo => 'foo', :bar => 'bar', :baz => 'baz' }
+    expected = { :foo => 'foo', :bar => 'bar' }
+    assert_equal expected, hash.slice(:foo, :bar)
+  end
+
+  test 'Hash#slice does not explode on a missing key' do
+    hash = {}
+    expected = {}
+    assert_equal expected, hash.slice(:foo)
   end
 end
