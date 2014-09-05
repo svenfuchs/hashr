b1c819e92f05f75d3ebd89a4c0cc737147636577
John Faucett <jwaterfaucett@gmail.com>
2014-09-06 00:32:26 +0200

    Fix require relative for ruby 1.8.7

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 require 'minitest/autorun'
 require 'test_declarative'
 require 'hashr'
-require_relative 'test_support'
+require 'test_support'
 
 module Minitest
   class Test
